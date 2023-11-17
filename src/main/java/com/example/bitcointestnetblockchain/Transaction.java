365f60f9899e2075dad91d90e9e3b6c6e2fb6971
nealtheseal108 <nealtheseal108@gmail.com>
2023-11-17 13:59:39 -0800

    q

--- src/main/java/com/example/bitcointestnetblockchain/Transaction.java
+++ src/main/java/com/example/bitcointestnetblockchain/Transaction.java
@@ -1,8 +1,10 @@
 package com.example.bitcointestnetblockchain;
 
 public class Transaction {
-    public int fromAddress;
-    public int toAddress;
+    public long fromAddress;
+    public long toAddress;
+    public long minerAddress;
+    public int transferAmount;
     long transactionHash;
     public Transaction() {
     }
