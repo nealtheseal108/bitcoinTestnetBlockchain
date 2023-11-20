6f7e6eb6835c9b4955fce6f307fe5474ca30e234
nealtheseal108 <nealtheseal108@gmail.com>
2023-11-20 14:00:52 -0800

    q

---
+++ src/main/java/com/example/bitcointestnetblockchain/binaryTreeExercises.java
@@ -0,0 +1,24 @@
+package com.example.bitcointestnetblockchain;
+
+public class binaryTreeExercises {
+    public static void main(String[] args) {
+
+    }
+
+    public static void intTreeNode() {
+
+    }
+}
+
+class intTreeNode {
+    private int payload;
+    private intTreeNode leftNode;
+    private intTreeNode rightNode;
+    public intTreeNode(int payload, intTreeNode leftNode, intTreeNode rightNode) {
+
+    }
+}
+
+class intTree {
+
+}
