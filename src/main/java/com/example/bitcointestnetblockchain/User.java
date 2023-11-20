4ba0bd6cb62f157b14419b031a4bbb46253469e6
nealtheseal108 <nealtheseal108@gmail.com>
2023-11-20 13:32:30 -0800

    q

---
+++ src/main/java/com/example/bitcointestnetblockchain/User.java
@@ -0,0 +1,10 @@
+package com.example.bitcointestnetblockchain;
+
+public class User {
+    private long username;
+    public long addressHash;
+    public boolean isMining;
+    public int balance;
+    public User() {
+    }
+}
