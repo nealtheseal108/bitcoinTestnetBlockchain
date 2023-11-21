48b36edc59d9e2fe9823b90f5c26de470429cc65
nealtheseal108 <nealtheseal108@gmail.com>
2023-11-21 13:50:58 -0800

    q

--- src/main/java/com/example/bitcointestnetblockchain/binaryTreeExercises.java
+++ src/main/java/com/example/bitcointestnetblockchain/binaryTreeExercises.java
@@ -1,5 +1,7 @@
 package com.example.bitcointestnetblockchain;
 
+import java.util.ArrayList;
+
 public class binaryTreeExercises {
     public static void main(String[] args) {
 
@@ -15,10 +17,37 @@ class intTreeNode {
     private intTreeNode leftNode;
     private intTreeNode rightNode;
     public intTreeNode(int payload, intTreeNode leftNode, intTreeNode rightNode) {
+        this.payload = payload;
+        this.leftNode= leftNode;
+        this.rightNode = rightNode;
+    }
 
+    public int getPayload() {
+        return payload;
+    }
+
+    public void setPayload(int payload) {
+        this.payload = payload;
+    }
+
+    public intTreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(intTreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public intTreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(intTreeNode rightNode) {
+        this.rightNode = rightNode;
     }
 }
 
 class intTree {
-
+    public intTree(ArrayList<Integer> treeListInOrder) {
+    }
 }
